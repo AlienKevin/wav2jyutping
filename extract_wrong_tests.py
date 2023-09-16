e6397b372851201838b4d1e2dd0e3812d245b63a
Kevin Li <kevinli020508@gmail.com>
2023-09-16 10:36:32 -0400

    Add whisper-small-jyutping-without-tones-full model

--- extract_wrong_tests.py
+++ extract_wrong_tests.py
@@ -1,4 +1,4 @@
-with open("whisper-base-jyutping-without-tones-full-test-output.txt", "r") as f:
+with open("whisper-small-jyutping-without-tones-full-test-output.txt", "r") as f:
     lines = f.readlines()
     for i in range(0, len(lines), 3):
         if lines[i] != lines[i + 1]:
